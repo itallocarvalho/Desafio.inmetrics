57c9dec1efbee6052a79a9676a7969feed551140
Itallcarvalho <italocarvalho.costa@hotmail.com>
2019-10-18 17:29:05 -0300

    aaa

--- src/main/java/com/jessila/repositories/LivroRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jessila.repositories;
-
-import com.jessila.model.Livro;
-
-public interface LivroRepository {
-
-	public boolean inserir(Livro livro);
-}
--- src/main/java/com/jessila/services/LivroService.java
+++
@@ -1,8 +0,0 @@
-package com.jessila.services;
-
-import com.jessila.model.Livro;
-
-public interface LivroService {
-
-	public boolean inserir(Livro livro);
-}
